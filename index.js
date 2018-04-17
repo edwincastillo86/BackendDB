843d82da0b5ae1e7a0e9c2cd8695bac881a70fa2
edwin castillo <edn_hack@hotmail.com>
2018-04-17 17:44:04 -0500

    metodo post test

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 var express = require('express')
 var app = express()
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
@@ -8,10 +12,24 @@ app.get('/', function(request, response) {
     response.send('Hello World!')
 })
 
-app.get('/registro', function(request, response) {
-    response.send('Registro!')
+app.get('/registro:email', function(request, response) {
+    var parametros = request.params.email;
+    response.send('Registro!' + parametros)
 })
+
+
+
+// parameters sent with
+app.post('/crearuser', function(req, res) {
+    var nombre = req.body.nombre;
+    var email = req.body.email;
+
+    res.send('Datos: ' + nombre + ' ' + email);
+});
+
 
 app.listen(app.get('port'), function() {
     console.log("Node app is running at localhost:" + app.get('port'))
 })
+
+
