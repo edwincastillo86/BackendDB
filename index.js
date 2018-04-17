d1b7dbac2cc99b442df77f8f996ffddf55833b8c
edwin castillo <edn_hack@hotmail.com>
2018-04-17 16:00:17 -0500

    pruebas rutes

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-/**
- * Created by egcastillo on 17/04/2018.
- */
+var express = require('express')
+var app = express()
+
+app.set('port', (process.env.PORT || 3000))
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', function(request, response) {
+    response.send('Hello World!')
+})
+
+app.get('/registro', function(request, response) {
+    response.send('Registro!')
+})
+
+app.listen(app.get('port'), function() {
+    console.log("Node app is running at localhost:" + app.get('port'))
+})
